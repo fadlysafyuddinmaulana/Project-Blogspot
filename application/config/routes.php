d9b79e4cfecfb88356a8e2490a498e5b03246caa
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-01-08 23:45:18 +0700

    Add File

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -49,6 +49,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['Register-Customer']     = 'Starter_Page/Register';
+$route['Login-Customer']        = 'Starter_Page/Index';
+
+$route['Profile-Customer']        = 'External_Page/Index';
+
+$route['Home']                  = 'Home';
+$route['Rumah']                 = 'Home/Rumah';
+$route['Tanah']                 = 'Home/Tanah';
+
+$route['Detail-Rumah']          = 'External_Page/Detail_Product_Rumah';
+$route['Detail-Tanah']          = 'External_Page/Detail_Product_Tanah';
