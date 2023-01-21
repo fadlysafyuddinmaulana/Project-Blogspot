376527301f61639070721468f194a5c0ba52ed2d
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-01-21 17:51:56 +0700

    new revision

--- application/views/Page-Styles/Navbar-Content.php
+++ application/views/Page-Styles/Navbar-Content.php
@@ -10,12 +10,6 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
                 </ul>
             </div>
         </div>
