fab0ab6f85076fba6b96a0556dc38060b4b49a64
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-01-22 21:14:27 +0700

    new rename code content web

--- application/views/Layout-Website/Content/Content-Web.php
+++ application/views/Layout-Website/Content/Content-Web.php
@@ -2,21 +2,13 @@
     <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <div class="col">
+            <div style="width: 100%;" class="col">
                 <div class="card shadow-sm">
                     <img src="https://images4.alphacoders.com/116/1160925.jpg" class="bd-placeholder-img card-img-top" width="100%" height="350" xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <div class="card-body">
                         <p class="card-text text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
                             voluptatibus officiis incidunt provident nostrum obcaecati vitae. Eos aperiam perspiciatis eligendi temporibus
                             omnis nihil, nesciunt officia iusto est non asperiores cumque enim culpa modi in ipsa.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <a class="btn btn-sm btn-outline-secondary">View</a>
-                            </div>
-                            <small class="text-muted">
-                                <?= date('d/m/Y'); ?>
-                            </small>
-                        </div>
                     </div>
                 </div>
             </div>
